3621dbd3cfa4eeffbf0bca830465f8b3d215291e
Dorota Kazak <dorota.kazak@gmail.com>
2018-05-07 11:31:55 +0200

    Add to commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var list = document.getElementById('list'),
+	add = document.getElementById('addElem');
+add.addEventListener('click', function() {
+	var element = document.createElement('li');
+/*Paamietaj że kolejność ma znaczenie bo inaczej liczba się nie wyświetli, var liLength musi byc przed element.inner*/
+	var liLength = document.getElementsByTagName('li').length;
+	element.innerHTML = 'item ' + [liLength];
+	list.appendChild(element);
+});
